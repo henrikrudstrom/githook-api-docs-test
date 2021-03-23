41af8329ba353fb3eabfce92fa49503540afedfb
Henrik Rudstrøm <henrik.rudstrom@amedia.no>
2021-03-23 21:15:09 +0100

    debug echos

--- github_actions/generate_slate_docs_action/build_api_docs.sh
+++ github_actions/generate_slate_docs_action/build_api_docs.sh
@@ -1,3 +1,8 @@
+echo "running slatedocs"
+echo "base folder ${DOC_BASE_FOLDER}"
+echo "output folder ${DOC_OUTPUT_FOLDER}"
+echo "pwd: $(pwd)"
+
 WORKDIR=$(pwd)
 cp -r $DOC_BASE_FOLDER/* /srv/slate/source
 cd /srv/slate
